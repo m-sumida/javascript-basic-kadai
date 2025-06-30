8b98cd3c8baf4f4996537f97cb5d208de6ec1c1a
m-sumida <m.sumida650355@gmail.com>
2025-06-30 21:55:41 +0900

    課題提出用

---
+++ kadai_022/event.js
@@ -0,0 +1,14 @@
+// 変更対象のHTML要素を取得
+const target = document.getElementById('text');
+
+// クリック対象のHTML要素を取得
+const btn = document.getElementById('btn');
+
+// クリック時の処理追加
+btn.addEventListener('click',()=>{
+
+    setTimeout(()=>{
+        // テキスト変更
+        target.textContent = 'ボタンをクリックしました';
+    },2000);
+});
